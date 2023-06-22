e2460e3326b3cf3f3332c0f6218d49a0a90b4e49
PaoloCarmine1201 <paolocarmine1201@gmail.com>
2023-06-22 11:38:16 +0200

    add modal "modal_riscatta_premio" for premi page bug fixes

--- app/src/main/java/com/example/unisegnala/PremiActivity.java
+++ app/src/main/java/com/example/unisegnala/PremiActivity.java
@@ -1,6 +1,9 @@
 package com.example.unisegnala;
 
+import android.app.Dialog;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,15 +13,20 @@ import androidx.fragment.app.Fragment;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class PremiActivity extends Fragment {
+    Dialog dialog;
 
     public PremiActivity() {
         // Costruttore vuoto richiesto
@@ -42,10 +50,18 @@ public class PremiActivity extends Fragment {
         int end = message.length();
 
         // Creazione di un ForegroundColorSpan per impostare il colore del numero
-        ForegroundColorSpan colorSpan = new ForegroundColorSpan(Color.RED);
+        ForegroundColorSpan colorSpan = new ForegroundColorSpan(Color.parseColor("#DF405C"));
 
         // Applicazione dello stile al numero specificando l'intervallo di caratteri
         spannableString.setSpan(colorSpan, start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        int start2 = 0;
+        int end2 = message.indexOf(":") + 1;
+
+        ForegroundColorSpan colorSpan2 = new ForegroundColorSpan(Color.BLACK);
+        spannableString.setSpan(colorSpan2, start2, end2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new StyleSpan(Typeface.BOLD), start2, end2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
 
         // Impostazione del testo nel TextView
         textView.setText(spannableString);
@@ -57,19 +73,44 @@ public class PremiActivity extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        dialog = new Dialog(getContext());
 
         ListView listView = view.findViewById(R.id.listaPremi);
 
         ArrayList<Premio> premi = new ArrayList<>();
 
-        premi.add(new Premio("5% sconto sulle fotocopie","250"));
-        premi.add(new Premio("Borraccia gratuita in associazione","750"));
-
+        premi.add(new Premio("5% sconto sulle fotocopie","250 punti"));
+        premi.add(new Premio("Borraccia gratuita in associazione","750 punti"));
 
         AdapterPremio adapterPremio = new AdapterPremio(getContext(),premi);
         listView.setAdapter(adapterPremio);
 
+        Button button = view.findViewById(R.id.buttonRiscatta);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openModal();
+            }
+        });
+
     }
 
+    private void openModal(){
+        dialog.setContentView(R.layout.modal_riscatta_premio);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button buttonChiudi = dialog.findViewById(R.id.buttonChiudi);
+        buttonChiudi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+
+
+    }
 
 }
